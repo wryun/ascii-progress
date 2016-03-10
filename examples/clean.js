082234251fc0ed1a8bf5ada7b2847a09c1c9e28e
bubkoo <bubkoo@163.com>
2016-03-10 17:57:07 +0800

    update clean example

--- examples/clean.js
+++ examples/clean.js
@@ -2,6 +2,7 @@ var ProgressBar = require('../index.js');
 
 var bar = new ProgressBar({
   clean   : true,
+  schema  : ' [:bar] \n:current/:total \n:percent \n:elapseds :etas',
   callback: function () {
     console.log(123);
   }
